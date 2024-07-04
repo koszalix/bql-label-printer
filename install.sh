796ba7dc8e5dde5ef8cc6861b5eda6e9d24808ad
pawel koszalka <koszalix@gmail.com>
2024-07-04 21:29:17 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 CWD=$(pwd)
 
-mkdir -p /usr/bin/bql-label-printer
+mkdir -p /opt/bql-label-printer
+mkdir -p /etc/bql-label-printer
+
 apt install python3
 apt install python3.11-venv
 
-cp -r . /usr/bin/bql-label-printer
-
-cd /usr/bin/bql-label-printer || exit 
+cp -r . /opt/bql-label-printer
+cd /opt/bql-label-printer || exit 
 
 python3 -m venv .env 
 source .env/bin/activate
 pip install -r requirements.txt 
 
 cp bql.service /etc/systemd/system/bql.service
+cp bql.yml /etc/bql-label-printer/
 
 systemctl enable bql
 systemctl start bql 
-
+systemctl status bql
